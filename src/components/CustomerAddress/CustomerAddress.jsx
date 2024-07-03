597965105224a9607da8f67ffd56ab23766e3ee6
Md-Hadiuzzaman-Rifat <hadirifat100@gmail.com>
2024-07-04 02:27:47 +0600

    button animation

--- src/components/CustomerAddress/CustomerAddress.jsx
+++ src/components/CustomerAddress/CustomerAddress.jsx
@@ -79,9 +79,9 @@ const orderStatus={name, phone ,email, address, division, orderedItem, date:new 
         <div>
           <label
             htmlFor="first-name"
-            className="block text-sm font-semibold leading-6 text-gray-900"
+            className="block text-md font-semibold leading-6 text-gray-900"
           >
-            Full Name
+            আপনার নাম <span className="text-red-600 text-md">*</span>
           </label>
           <div className="mt-2.5">
             <input
@@ -96,32 +96,15 @@ const orderStatus={name, phone ,email, address, division, orderedItem, date:new 
           </div>
         </div>
 
-        <div className="sm:col-span-2">
-          <label
-            htmlFor="email"
-            className="block text-sm font-semibold leading-6 text-gray-900"
-          >
-            Email
-          </label>
-          <div className="mt-2.5">
-            <input
-              type="email"
-              name="email"
-              id="email"
-              onChange={(e) => setEmail(e.target.value)}
-              autoComplete="email"
-              className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-            />
-          </div>
-        </div>
+
 
         <div className="sm:col-span-2">
           <label
             htmlFor="phone-number"
-            className="block text-sm font-semibold leading-6 text-gray-900"
+            className="block text-md font-semibold leading-6 text-gray-900"
           >
-            Phone number
-          </label>
+            ফোন নম্বর <span className="text-red-600 text-md font-bold">*</span>
+          </label> 
           <div className="relative mt-2.5">
             <div className="absolute inset-y-0 left-0 flex items-center">
               <label htmlFor="country" className="sr-only">
@@ -147,11 +130,31 @@ const orderStatus={name, phone ,email, address, division, orderedItem, date:new 
           </div>
         </div>
 
+        <div className="col-span-full">
+          <label
+            htmlFor="street-address"
+            className="block text-md font-bold leading-6 text-gray-900"
+          >
+           ঠিকানা <span className="font-bold text-lg text-red-600">*</span>
+          </label>
+          <div className="mt-2">
+            <input
+              onChange={(e) => setAddress(e.target.value)}
+              type="text"
+              name="street-address"
+              id="street-address"
+              autoComplete="street-address"
+              className="block pl-4 w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              required
+            />
+          </div>
+        </div>
+
         {/* // inside or outside dhaka  */}
 
         <div className="border my-4 p-4 rounded-md">
           <div className="flex flex-col gap-2">
-            <p className="font-bold">Shipping Address</p>
+            <p className="font-bold">শিপিং মেথড</p>
             <label>
               <input
                 onChange={(e) =>setDivision(e.target.value)}
@@ -160,7 +163,7 @@ const orderStatus={name, phone ,email, address, division, orderedItem, date:new 
                 name="myRadio"
                 value="isd"
               />{" "}
-              Dhaka
+              ঢাকার ভিতর 
             </label>
 
             <label>
@@ -171,36 +174,25 @@ const orderStatus={name, phone ,email, address, division, orderedItem, date:new 
                 checked={division === "osd"}
                 value="osd"
               />{" "}
-              Outside Dhaka
+              ঢাকার বাহির
             </label>
           </div>
         </div>
 
-        <div className="col-span-full">
-          <label
-            htmlFor="street-address"
-            className="block text-sm font-medium leading-6 text-gray-900"
-          >
-            Street address
-          </label>
-          <div className="mt-2">
-            <input
-              onChange={(e) => setAddress(e.target.value)}
-              type="text"
-              name="street-address"
-              id="street-address"
-              autoComplete="street-address"
-              className="block pl-4 w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-            />
-          </div>
-        </div>
+
         <div className="mt-2">
           <button
             type="submit"
-            className="block w-full rounded-md bg-indigo-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 mb-6"
+            className="block w-full rounded-md bg-indigo-600 px-3.5 py-2.5 text-center font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 mb-6 text-lg"
           >
-            Confirm Order
+            অর্ডার কনফর্ম করুন
           </button>
+          {/* <button
+            type="submit"
+            className="block w-full rounded-md bg-indigo-600 px-3.5 py-2.5 text-center font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 mb-6 text-lg"
+          >
+            অর্ডার কনফর্ম করুন
+          </button> */}
         </div>
       </form>
     </div>
